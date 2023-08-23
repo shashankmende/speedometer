ee946f92fb60094c8763e5983e9cfd0c27598bd6
shashank <shashankmende88@gmail.com>
2023-08-23 16:58:24 +0000

    speedometer3

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -2,16 +2,16 @@ import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
-  state = {speed: 0}
+  state: {speed: 0}
 
-  onIncrement = () => {
+  Accelerate = () => {
     const {speed} = this.state
     if (speed < 200) {
       this.setState(prevState => ({speed: prevState.speed + 10}))
     }
   }
 
-  onDecrement = () => {
+  Break = () => {
     const {speed} = this.state
     if (speed > 0) {
       this.setState(prevState => ({speed: prevState.speed - 10}))
@@ -19,7 +19,7 @@ class Speedometer extends Component {
   }
 
   render() {
-    const {speed} = this.speed
+    const {speed} = this.state
     return (
       <div className="bg_container">
         <h1 className="heading">SPEEDOMETER</h1>
@@ -28,19 +28,21 @@ class Speedometer extends Component {
           className="image"
           alt="speedometer"
         />
-        <h1 className="heading">Speed is {speed}mph</h1>
-        <p className="heading">Min Limit is 0mph,Max Limit is 200mph</p>
-        <div className="buttons-container">
-          <button
-            type="button"
-            className="Accelerate"
-            onClick={this.onIncrement}
-          >
-            Accelerate
-          </button>
-          <button type="button" className="Break" onClick={this.onDecrement}>
-            Break
-          </button>
+        <div>
+          <h1 className="heading">Speed is {speed}mph</h1>
+          <p className="heading">Min Limit is 0mph, Max Limit is 200mph</p>
+          <div className="buttons-container">
+            <button
+              type="button"
+              className="Accelerate"
+              onClick={this.Accelerate}
+            >
+              Accelerate
+            </button>
+            <button type="button" className="Break" onClick={this.Break}>
+              Apply Brake
+            </button>
+          </div>
         </div>
       </div>
     )
